1b2714a5b87825febafa1e3fb595b758030eec40
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2023-02-19 15:10:31 +0530

    form-validation-reactjs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,114 @@
 import React, { useState } from 'react';
 
-const SearchList = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [list, setList] = useState([
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Another item' },
-    { id: 4, name: 'Yet another item' },
-  ]);
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
 
-  const filteredList = list.filter((item) =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validateForm();
+  };
+
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (!formData.name) {
+      isValid = false;
+      errors.name = 'Please enter your name';
+    }
+
+    if (!formData.email) {
+      isValid = false;
+      errors.email = 'Please enter your email';
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      isValid = false;
+      errors.email = 'Please enter a valid email address';
+    }
+
+    if (!formData.password) {
+      isValid = false;
+      errors.password = 'Please enter a password';
+    } else if (formData.password.length < 6) {
+      isValid = false;
+      errors.password = 'Password must be at least 6 characters';
+    }
+
+    if (!formData.confirmPassword) {
+      isValid = false;
+      errors.confirmPassword = 'Please confirm your password';
+    } else if (formData.confirmPassword !== formData.password) {
+      isValid = false;
+      errors.confirmPassword = 'Passwords do not match';
+    }
+
+    setFormErrors(errors);
+    return isValid;
+  };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <ul>
-        {filteredList.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        {formErrors.name && <span>{formErrors.name}</span>}
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {formErrors.email && <span>{formErrors.email}</span>}
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        {formErrors.password && <span>{formErrors.password}</span>}
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+        {formErrors.confirmPassword && <span>{formErrors.confirmPassword}</span>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
-export default SearchList;
+export default Form;
